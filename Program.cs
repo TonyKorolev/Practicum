5958b9534e94d97540f24cc1b95b3971ef7aec98
KorolevAV <antoshka.992@mail.ru>
2022-02-02 20:53:16 +0300

    add void PrintArray

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintArray(int[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("Массив пустой!");
+        return;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
