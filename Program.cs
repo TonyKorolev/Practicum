b9ce1cd7803150686bae0ccc0a8e3232a54b5dc5
KorolevAV <antoshka.992@mail.ru>
2022-02-02 20:54:42 +0300

    add int [] GenerateNewArray

--- Program.cs
+++ Program.cs
@@ -21,3 +21,31 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
+int EvenCounter(int[] array)
+{
+    int evenCounter = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0)
+        {
+            evenCounter++;
+        }
+    }
+    return evenCounter;
+}
+
+int[] GenerateNewArray(int[] array)
+{
+    int[] resultArray = new int[EvenCounter(array)];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
